5ca3e4f6faf055668d5ff8776f87a15b343d2270
Maciej Ślusarz <maciej.slusarz@proton.me>
2022-12-08 20:32:32 +0100

    Dodanie komentarza dot. działania rekurencji ogonowej

--- src/python/fibonacci_ogonowo.py
+++ src/python/fibonacci_ogonowo.py
@@ -23,9 +23,34 @@ def element_fibonacciego(n, a=1, b=1):
     # a wartość b zamieniamy na sumę pierwotnego a i pierwotnego b
     return element_fibonacciego(n - 1, b, a + b)
 
+
 def main():
     print(element_fibonacciego(40))
+    print(element_fibonacciego(8))
     print(element_fibonacciego(1))
+
+    """
+    wywołujemy fibonacci_ogonowo(8)
+
+    fibonacci_ogonowo(8) 
+        -> fibonacci_ogonowo(8-1, 1, 1+1)
+                          -----
+                         \/
+        -> fibonacci_ogonowo(7-1, 2, 2+1)
+                          -----
+                         \/
+        -> fibonacci_ogonowo(6-1, 3, 3+2)
+                          -----
+                         \/
+        -> fibonacci_ogonowo(5-1, 5, 5+3)
+                          -----
+                         \/
+        -> fibonacci_ogonowo(4-1, 8, 8+5)
+                          -----
+                         \/
+        -> fibonacci_ogonowo(3-1, 13, 13+8)
+        -> zwróć b (b = 21=13+8) bo n = 2
+    """
 
 
 if __name__ == "__main__":
